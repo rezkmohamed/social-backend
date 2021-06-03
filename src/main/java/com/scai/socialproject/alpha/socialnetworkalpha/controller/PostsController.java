6bee56662be0d49258ffa8616c10b4b1793a17d2
Mohamed Rezk <Mohamed.Rezk@scai.intra>
2021-06-03 10:04:25 +0200

    add post with token security added

--- src/main/java/com/scai/socialproject/alpha/socialnetworkalpha/controller/PostsController.java
+++ src/main/java/com/scai/socialproject/alpha/socialnetworkalpha/controller/PostsController.java
@@ -1,12 +1,15 @@
 package com.scai.socialproject.alpha.socialnetworkalpha.controller;
 
+import java.io.IOException;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +22,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.scai.socialproject.alpha.socialnetworkalpha.dto.PostDTO;
 import com.scai.socialproject.alpha.socialnetworkalpha.service.PostService;
+import com.scai.socialproject.alpha.socialnetworkalpha.utils.RequestUtils;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
@@ -40,16 +48,22 @@ public class PostsController {
 	
 	//OK!
 	@GetMapping("/homepage/{id}")
-	public List<PostDTO> getHomepage(@PathVariable String id, HttpServletRequest request){
-		/*System.out.println("controller");
-		String header = request.getHeader("Authorization");
-		System.out.println("header: " + header);*/
-		return postService.getHomepage(id);
+	public ResponseEntity<List<PostDTO>> getHomepage(@PathVariable String id, HttpServletRequest request, HttpServletResponse response ) throws IOException{
+		int position = request.getRequestURI().lastIndexOf("/");
+		String idRequest = request.getRequestURI().substring(position + 1);
+		String idProfile = RequestUtils.idProfileFromToken(request);
+		
+		if(!idRequest.equals(idProfile)) {
+			response.sendError(401);
+			return new ResponseEntity<>(null, HttpStatus.UNAUTHORIZED);
+		}
+		
+		return new ResponseEntity<>(postService.getHomepage(id), HttpStatus.OK);
 	}
 	
 	//OK!
 	@GetMapping("/{idPost}")
-	public PostDTO findPostById(@PathVariable String idPost) {
+	public PostDTO findPostById(@PathVariable String idPost, HttpServletRequest request, HttpServletResponse respons) {
 		PostDTO post = postService.findPostById(idPost);
 		if(post == null) {
 			throw new RuntimeException("ERROR - POST WITH ID: " + idPost + " NOT FOUND");
@@ -58,10 +72,16 @@ public class PostsController {
 	}
 	
 	//OK!
-	@PostMapping("")
-	public PostDTO addPost(@RequestBody PostDTO postDTO) {
+	@PostMapping("/newpost")
+	public ResponseEntity<PostDTO> addPost(@RequestBody(required = false) PostDTO postDTO, HttpServletRequest request, HttpServletResponse response) {
+		/**
+		 * FORCING THE ID OF PROFILE WITH TOKEN
+		 */
+		String idProfile = RequestUtils.idProfileFromToken(request);
+		postDTO.setIdProfile(idProfile);
+		
 		postService.savePost(postDTO);
-		return postDTO;
+		return new ResponseEntity<>(postDTO, HttpStatus.OK);
 	}
 	
 	//OK!
@@ -71,7 +91,7 @@ public class PostsController {
 	}
 	
 	@PutMapping("/{idPost}")
-	public ResponseEntity<HttpStatus> updatePost(@RequestBody PostDTO postDTO){
+	public ResponseEntity<HttpStatus> updatePost(@RequestBody(required = false) PostDTO postDTO) throws HttpMessageNotReadableException{
 		return postService.updatePost(postDTO);
 	}
 }
--- src/main/java/com/scai/socialproject/alpha/socialnetworkalpha/filter/PostsFilter.java
+++ src/main/java/com/scai/socialproject/alpha/socialnetworkalpha/filter/PostsFilter.java
@@ -1,14 +1,14 @@
 package com.scai.socialproject.alpha.socialnetworkalpha.filter;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
 
-import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -22,7 +22,7 @@ import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 
 
-@Component
+//@Component
 @Order(Ordered.LOWEST_PRECEDENCE)
 public class PostsFilter extends OncePerRequestFilter{
 	List<String> requestsWithIdRequired = new LinkedList<>();
@@ -30,7 +30,6 @@ public class PostsFilter extends OncePerRequestFilter{
 	
 	{
 		requestsWithIdRequired.add("/posts/homepage");
-		requestsWithIdRequired.add("/profiles/newpassword");
 		
 		
 		requestsWithoutIdRequired.add("/posts");
@@ -47,13 +46,12 @@ public class PostsFilter extends OncePerRequestFilter{
 			Jws<Claims> result =  Jwts.parser().setSigningKey("ciao").parseClaimsJws(token);
 
 			int position = request.getRequestURI().lastIndexOf("/");
-			//System.out.println(position);
 			String requestSubstring = request.getRequestURI().substring(0, position);
 			String idRequest = request.getRequestURI().substring(position + 1);
-			//System.out.println(idRequest);
-			//System.out.println(requestSubstring);
+			String idProfile = result.getBody().get("idUser", String.class);
+
+			System.out.println(requestSubstring);
 			if(requestsWithIdRequired.contains(requestSubstring)) {
-				String idProfile = result.getBody().get("idUser", String.class);
 				if(!idRequest.equals(idProfile)) {
 					response.sendError(401);
 					return;
@@ -61,13 +59,58 @@ public class PostsFilter extends OncePerRequestFilter{
 				System.out.println("la chiamata è sicura");
 				filterChain.doFilter(request, response);
 				return;
+			} 
+			
+			else if(requestsWithoutIdRequired.contains(requestSubstring)) {
+				if("POST".equalsIgnoreCase(request.getMethod()) || "PUT".equalsIgnoreCase(request.getMethod())) {
+					System.out.println("request without id required");
+					request.getAttribute("post");
+
+					Map<String, String> bodyResponse =  getBodyRequest(request);
+					String bodyResponseId = bodyResponse.get("\"idProfile\"");
+					bodyResponseId = bodyResponseId.substring(1, bodyResponseId.length()-1);
+					System.out.println(bodyResponseId);
+					if(!bodyResponseId.equals(idProfile)) {
+						response.sendError(401);
+						return;
+					}
+					
+					System.out.println("la chiamata è sicura");
+					filterChain.doFilter(request, response);
+					return;
+				}
 			}
 			
-			if(requestsWithoutIdRequired.contains(requestSubstring)) {
-				
-			}
-
+			
 		}		
 		filterChain.doFilter(request, response);
+	}
+	
+	static String extractPostRequestBody(HttpServletRequest request) {
+	    if ("POST".equalsIgnoreCase(request.getMethod())) {
+	        Scanner s = null;
+	        try {
+	            s = new Scanner(request.getInputStream(), "UTF-8").useDelimiter("\\A");
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	        return s.hasNext() ? s.next() : "";
+	    }
+	    return "";
+	}
+	
+	static Map<String, String> getBodyRequest(HttpServletRequest request) throws IOException {
+		Map<String, String> ris = new HashMap<>();
+		String s = extractPostRequestBody(request);
+		s = s.substring(1,s.length()-1);
+		System.out.println(s);
+		String[] arrayOfResponse = s.split(",");
+		
+		for(String response : arrayOfResponse) {
+			String[] couple = response.split(":");
+			ris.put(couple[0], couple[1]);
+		}
+		
+		return ris;
 	}
 }
