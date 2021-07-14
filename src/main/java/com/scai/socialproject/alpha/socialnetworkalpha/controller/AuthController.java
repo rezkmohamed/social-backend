9adab97382ab130ad98200438d2860bd92136e03
Mohamed Rezk <Mohamed.Rezk@scai.intra>
2021-07-14 16:09:38 +0200

    init

--- src/main/java/com/scai/socialproject/alpha/socialnetworkalpha/controller/AuthController.java
+++ src/main/java/com/scai/socialproject/alpha/socialnetworkalpha/controller/AuthController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -31,6 +32,12 @@ public class AuthController {
 	private RequestUtils requestUtils;
 	@Value("${signingKey}")
 	private String signingKey;
+	
+	
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "Wlcome to spring boot heroku demo";
+	}
 
 	@PostMapping("/login")
 	public ResponseEntity<User> login(@RequestBody User user){	
